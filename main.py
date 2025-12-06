6c5b75a170b029ec469302dd77ea8be7ba1b0101
NyaLYSM <hglot10@gmail.com>
2025-12-06 20:57:41 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from database import Base, engine
+from routers import auth, wardrobe, looks, profile
+from models import Base
+from database import engine
+
+Base.metadata.create_all(bind=engine)
+
+
+app = FastAPI(title="Stylist Backend API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Attach routers
+app.include_router(auth.router, prefix="/api/auth", tags=["auth"])
+app.include_router(wardrobe.router, prefix="/api/wardrobe", tags=["wardrobe"])
+app.include_router(looks.router, prefix="/api/looks", tags=["looks"])
+app.include_router(profile.router, prefix="/api/profile", tags=["profile"])
+
+
+@app.get("/")
+def home():
+    return {"status": "ok", "message": "Backend работает!"}
+
+
+@app.get("/health")
+def health():
+    return {"status": "healthy"}
